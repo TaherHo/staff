a9298185b4d0a14c45d3f285e421c80a721ff2a8
taHo <hoseininejad@gmail.com>
2017-08-09 00:54:26 +0200

    Added 'Load more' button.

--- client/components/staffDetails.js
+++ client/components/staffDetails.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const StaffDetails = ({details}) => {
-    console.log(details);
     const {name, email, phone, avatar} = details;
 
     return (
--- client/components/staffList.js
+++ client/components/staffList.js
@@ -3,16 +3,19 @@ import StaffDetails from './staffDetails';
 import {createContainer} from 'react-meteor-data';
 import {StaffCol} from '../../imports/collections/staffCol';
 
-const StaffList = (props) =>{
-    console.log(props.staffCol);
-    return(
-        <div className="staff-list">
-            {props.staffCol.map(person => <StaffDetails key={person._id} details={person}/>)}
+const StaffList = (props) => {
+    return (
+        <div>
+            <div className="staff-list">
+                {props.staffCol.map(person => <StaffDetails key={person._id} details={person}/>)}
+            </div>
+            <button onClick={() => console.log('clicked')} className="btn btn-large btn-primary"> Load more</button>
         </div>
+
     );
 };
 
-export default createContainer(() =>{
-    Meteor.subscribe('staffCol' , 20);
-    return { staffCol:  StaffCol.find({}).fetch() };
-},StaffList);
+export default createContainer(() => {
+    Meteor.subscribe('staffCol', 20);
+    return {staffCol: StaffCol.find({}).fetch()};
+}, StaffList);
