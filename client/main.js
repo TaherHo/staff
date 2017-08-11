635bcc17096f60b52125a620b469159e3afd169f
taHo <hoseininejad@gmail.com>
2017-08-11 12:43:51 +0200

    Added contactList component.

--- client/main.js
+++ client/main.js
@@ -2,9 +2,11 @@ import React from 'react';
 import reactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
 
+import ContactList from './components/contactList';
+
 const App = ()=> {
     return(<div>
-        hi
+        <ContactList/>
     </div>);
 };
 
